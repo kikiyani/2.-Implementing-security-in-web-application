b2d395b4e74060e38b93300fa4dc77bf149f84ad
kikiyani <153810235+kikiyani@users.noreply.github.com>
2025-07-29 06:27:44 +0500

    server.js

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-// const csrf = require('csurf');
+const csrf = require('csurf');
 const consolidate = require("consolidate"); // Templating library adapter for Express
 const swig = require("swig");
 const helmet = require("helmet");
 const MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
 const http = require("http");
 const marked = require("marked");
-//const nosniff = require('dont-sniff-mimetype');
+const nosniff = require('dont-sniff-mimetype');
 const app = express();
 app.use(helmet());
  // Web framework to handle routing requests
 const routes = require("./app/routes");
 const { port, db, cookieSecret } = require("./config/config"); // Application config properties
 /*
-// Fix for A6-Sensitive Data Exposure
 // Load keys for establishing secure HTTPS connection
 const fs = require("fs");
 const https = require("https");
@@ -37,9 +36,9 @@ MongoClient.connect(db, (err, db) => {
     }
     console.log(`Connected to the database`);
 
-    /*
-    // Fix for A5 - Security MisConfig
-    // TODO: Review the rest of helmet options, like "xssFilter"
+    
+   
+    
     // Remove default x-powered-by response header
     app.disable("x-powered-by");
 
@@ -55,16 +54,11 @@ MongoClient.connect(db, (err, db) => {
     // Allow communication only on HTTPS
     app.use(helmet.hsts());
 
-    // TODO: Add another vuln: https://github.com/helmetjs/helmet/issues/26
-    // Enable XSS filter in IE (On by default)
-    // app.use(helmet.iexss());
-    // Now it should be used in hit way, but the README alerts that could be
-    // dangerous, like specified in the issue.
-    // app.use(helmet.xssFilter({ setOnOldIE: true }));
+    
 
     // Forces browser to only use the Content-Type set in the response header instead of sniffing or guessing it
     app.use(nosniff());
-    */
+    
 
     // Adding/ remove HTTP Headers for security
     app.use(favicon(__dirname + "/app/assets/favicon.ico"));
@@ -85,26 +79,17 @@ MongoClient.connect(db, (err, db) => {
         // Both mandatory in Express v4
         saveUninitialized: true,
         resave: true
-        /*
-        // Fix for A5 - Security MisConfig
-        // Use generic cookie name
-        key: "sessionId",
-        */
-
-        /*
-        // Fix for A3 - XSS
-        // TODO: Add "maxAge"
+        
         cookie: {
             httpOnly: true
             // Remember to start an HTTPS server to get this working
             // secure: true
         }
-        */
-
+       
     }));
 
-    /*
-    // Fix for A8 - CSRF
+  
+   
     // Enable Express csrf protection
     app.use(csrf());
     // Make csrf token available in templates
@@ -112,7 +97,7 @@ MongoClient.connect(db, (err, db) => {
         res.locals.csrftoken = req.csrfToken();
         next();
     });
-    */
+   
 
     // Register templating engine
     app.engine(".html", consolidate.swig);
